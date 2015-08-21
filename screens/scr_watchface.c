8729f65fc5684fe0b7314a5841bcee87a76a8279
Roel van Westerop <roel.van.westerop@medtronic.com>
2015-08-21 13:35:56 +0200

    Updated watchface

--- screens/scr_watchface.c
+++ screens/scr_watchface.c
@@ -13,7 +13,7 @@ static const SCR_CONTROL_NUMBER_CONFIG hour_config = {
 		NUMBER_RANGE_0__99,
 	  4,
 	  4,
-	  NUMBER_FORMAT_FLAG_ZERO_PADDED | 4 << 22 | 8 << 16 | 66 << 8 | 50,
+	  NUMBER_FORMAT_FLAG_ZERO_PADDED | 4 << 22 | 4 << 16 | 24 << 8 | 50,
 	  (uint32_t (*)(uint32_t, uint8_t))rtc_get_current_hour,
 	  0,
     &hour_ctrl_data
@@ -23,12 +23,24 @@ static NUMBER_CONTROL_DATA minutes_ctrl_data;
 
 static const SCR_CONTROL_NUMBER_CONFIG minutes_config = {
 		NUMBER_RANGE_0__99,
+	  60,
 	  4,
-	  59,
-	  NUMBER_FORMAT_FLAG_ZERO_PADDED | 4 << 22 | 6 << 16 | 66 << 8 | 50,
+	  NUMBER_FORMAT_FLAG_ZERO_PADDED | 4 << 22 | 4 << 16 | 24 << 8 | 50,
 	  (uint32_t (*)(uint32_t, uint8_t))rtc_get_current_minutes,
 	  0,
     &minutes_ctrl_data
+};
+
+static NUMBER_CONTROL_DATA seconds_ctrl_data;
+
+static const SCR_CONTROL_NUMBER_CONFIG seconds_config = {
+	NUMBER_RANGE_0__99,
+	  114,
+	  24,
+	  NUMBER_FORMAT_FLAG_ZERO_PADDED | 4 << 22 | 4 << 16 | 12 << 8 | 30,
+	  (uint32_t (*)(uint32_t, uint8_t))rtc_get_current_seconds,
+	  0,
+    &seconds_ctrl_data
 };
 
 static NUMBER_CONTROL_DATA days_ctrl_data;
@@ -54,26 +66,12 @@ static const SCR_CONTROL_NUMBER_CONFIG months_config = {
 };
 
 
-static NUMBER_CONTROL_DATA seconds_ctrl_data;
-
-static const SCR_CONTROL_PROGRESS_BAR_CONFIG seconds_config = {
-	  0,
-	  MLCD_YRES - 3,
-	  MLCD_XRES,
-	  2,
-	  60,
-		0,
-	  rtc_get_current_seconds,
-	  0,
-    &seconds_ctrl_data
-};
-
 static const SCR_CONTROL_DEFINITION controls[] = {
 	  {SCR_CONTROL_NUMBER, (void*)&hour_config},
 		{SCR_CONTROL_NUMBER, (void*)&minutes_config},
+		{SCR_CONTROL_NUMBER, (void*)&seconds_config},
 		{SCR_CONTROL_NUMBER, (void*)&days_config},
-		{SCR_CONTROL_NUMBER, (void*)&months_config},
-		{SCR_CONTROL_HORIZONTAL_PROGRESS_BAR, (void*)&seconds_config}
+		{SCR_CONTROL_NUMBER, (void*)&months_config}		
 };
 
 static const SCR_CONTROLS_DEFINITION controls_definition = {
